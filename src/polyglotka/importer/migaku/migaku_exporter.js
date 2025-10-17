6bce4ed7679acf29df3c5854a01f6f51d61c7c65
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2025-10-17 22:54:48 +0600

    rename MME

--- src/polyglotka/importer/migaku/migaku_exporter.js
+++ src/polyglotka/importer/migaku/migaku_exporter.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Migaku deck exporter
+// @name        Migaku Memory Exporter
 // @namespace   Violentmonkey Scripts
 // @match       https://study.migaku.com/*
 // @grant       GM_getResourceURL
